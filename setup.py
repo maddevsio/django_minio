8c24064c30489b5e43dc63e62fefc835b68f2195
shapoglyk <abylov.belek@gmail.com>
2016-06-29 16:05:12 +0600

    Fix readme file extension in setup file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
