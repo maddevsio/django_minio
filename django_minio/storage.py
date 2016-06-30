d47bd07e58318ecc29893855badcc77f7f1ed82b
shapoglyk <abylov.belek@gmail.com>
2016-06-30 14:26:24 +0600

    Using streaming upload to Minio. New way to implement connection.

--- django_minio/storage.py
+++ django_minio/storage.py
@@ -1,46 +1,66 @@
-from django.core.files.storage import FileSystemStorage
+import sys
+from django.core.files.storage import Storage
 from django.utils.deconstruct import deconstructible
 from minio import Minio
-from minio.error import ResponseError
 from django.conf import settings
+import mimetypes
 
 
-minioClient = Minio(settings.MINIO_SERVER,
-                    access_key=settings.MINIO_ACCESSKEY,
-                    secret_key=settings.MINIO_SECRET,
-                    secure=True)
-
-bucket = settings.MINIO_BUCKET
+def setting(name, default=None):
+    """
+    Helper function to get a Django setting by name or (optionally) return
+    a default (or else ``None``).
+    """
+    return getattr(settings, name, default)
 
 
 @deconstructible
-class MinioStorage(FileSystemStorage):
+class MinioStorage(Storage):
+    server = setting('MINIO_SERVER')
+    access_key = setting('MINIO_ACCESSKEY')
+    secret_key = setting('MINIO_SECRET')
+    bucket = setting('MINIO_BUCKET')
+    secure = True
+
+    def __init__(self, *args, **kwargs):
+        super(MinioStorage, self).__init__(*args, **kwargs)
+        self._connection = None
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            self._connection = Minio(
+                self.server, self.access_key, self.secret_key, self.secure)
+        self._connection.trace_on(sys.stdout)
+        return self._connection
+
+    def _bucket_has_object(self, name):
+        # temporary returning always False
+        return False
 
     def _save(self, name, content):
-        """"
-        Override _save() method to load file object to Minio server.
-        """
-        obj = super(MinioStorage, self)._save(name, content)
-        try:
-            minioClient.make_bucket(bucket)
-        except ResponseError:
-            try:
-                file_path = settings.MEDIA_ROOT + '/' + obj
-                minioClient.fput_object(bucket, obj, file_path)
-            except ResponseError:
-                pass
-        return obj
+
+        print('SAVE BEGIN', name, content)
+        print('name: ', name)
+        print('content:', content)
+        if hasattr(content.file, 'content_type'):
+            content_type = content.file.content_type
+        else:
+            content_type = mimetypes.guess_type(name)[0]
+
+        # if hasattr(content, 'chunks'):
+        #     content_data = b''.join(chunk for chunk in content.chunks())
+        # else:
+        #     content_data = content.read()
+        print('content.file.size: ', content.file.size)
+        self.connection.put_object(self.bucket, name, content, content.file.size, content_type=content_type)
+        return name
 
     def url(self, name):
-        """"
-        Override url() method to get the url of a file from Minio server.
-        """
-        file_url = ''
-        try:
-            file_url = minioClient.presigned_get_object(bucket, name)
-        except ResponseError:
-            pass
-        return file_url
+        if self.connection.bucket_exists(self.bucket):
+            return self.connection.presigned_get_object(self.bucket, name)
+        else:
+            return "{}/{}".format(setting('MEDIA_URL'), name)
 
     def exists(self, name):
-        return super(MinioStorage, self).exists(name)
+        return self._bucket_has_object(name)
