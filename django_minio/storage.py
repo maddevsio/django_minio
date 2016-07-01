95efd89a05118b2b5cda1667617e9c1e925ee148
shapoglyk <abylov.belek@gmail.com>
2016-07-01 18:51:16 +0600

    Catch InvalidEndpointerror

--- django_minio/storage.py
+++ django_minio/storage.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.core.files.storage import Storage
 from django.utils.deconstruct import deconstructible
 from minio import Minio
-from minio.error import ResponseError, InvalidXMLError
+from minio.error import ResponseError, InvalidXMLError, InvalidEndpointError
 from urllib3.exceptions import MaxRetryError
 
 
@@ -20,6 +20,7 @@ def setting(name, default=None):
 
 @deconstructible
 class MinioStorage(Storage):
+    # TODO: Figure out how properly work with all exceptions which are raises bellow.
     server = setting('MINIO_SERVER')
     access_key = setting('MINIO_ACCESSKEY')
     secret_key = setting('MINIO_SECRET')
@@ -33,19 +34,24 @@ class MinioStorage(Storage):
     @property
     def connection(self):
         if self._connection is None:
-            print('construct')
-            self._connection = Minio(
-                self.server, self.access_key, self.secret_key, self.secure)
+            try:
+                self._connection = Minio(
+                    self.server, self.access_key, self.secret_key, self.secure)
+            except InvalidEndpointError:
+                self._connection = None
         # self._connection.trace_on(sys.stdout)
         return self._connection
 
     def _bucket_has_object(self, name):
-        try:
-            self.connection.get_object(self.bucket, name)
-            return True
-        except (ResponseError, MaxRetryError) as err:
-            # Exception rises when file not found.
-            return False
+        if self.connection is not None:
+            try:
+                self.connection.get_object(self.bucket, name)
+                return True
+            except (ResponseError, MaxRetryError) as err:
+                # ResponseError rises when file not found.
+                # MaxRetryError rises when service is not available.
+                pass
+        return False
 
     def _save(self, name, content):
         pathname, ext = os.path.splitext(name)
@@ -55,22 +61,25 @@ class MinioStorage(Storage):
             content_type = content.file.content_type
         else:
             content_type = mimetypes.guess_type(name)[0]
-        try:
-            self.connection.put_object(self.bucket, hashed_name, content, content.file.size, content_type=content_type)
-        except InvalidXMLError as err:
-            print(err)
-        except MaxRetryError:
-            pass
+        if self.connection is not None:
+            try:
+                self.connection.put_object(self.bucket, hashed_name, content, content.file.size, content_type=content_type)
+            except InvalidXMLError as err:
+                pass
+            except MaxRetryError:
+                pass
         return hashed_name
 
     def url(self, name):
-        try:
-            if self.connection.bucket_exists(self.bucket):
-                return self.connection.presigned_get_object(self.bucket, name)
-            else:
+        if self.connection is not None:
+            try:
+                if self.connection.bucket_exists(self.bucket):
+                    return self.connection.presigned_get_object(self.bucket, name)
+                else:
+                    return "image_not_found"
+            except MaxRetryError:
                 return "image_not_found"
-        except MaxRetryError:
-            return "image_not_found"
+        return "could_not_establish_connection"
 
     def exists(self, name):
         return self._bucket_has_object(name)
